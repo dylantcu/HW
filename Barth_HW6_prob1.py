a0ac980408a45fe05d5d2f735949f0cd5f5d5743
dylantcu <d.barth@tcu.edu>
2017-04-10 10:31:46 -0500

    Add files via upload

--- Barth_HW6_prob1.py
+++ Barth_HW6_prob1.py
@@ -8,17 +8,17 @@ Created on Wed Apr  5 09:41:16 2017
 import numpy
 from matplotlib import pyplot as p
 #initial conditions
-theta_0 = -177.
+theta_0 = 92.
 omega_0 = 0.
 length = .1
 #swtich params
-diff_method = "RK" # "RK"|"LF"|null
+diff_method = "LF" # "RK"|"LF"|null
      #RK produces the RungeKutta diff eq solution, and LF the leapfrog
 gif_method = 0 # 0|1
     #0 turns the gif maker off, 1 on.
 #steps and sizes
 start = 0.
-end =  50.
+end =  10.
 N = 2000
 h = (end-start)/N
 #set up arrays for various computations
@@ -52,16 +52,16 @@ if diff_method == "RK":
         k3 = h*vector(r_0 + .5*k2,t + .5*h)
         k4 = h*vector(r_0 + k3,t + h)
         r_0 += (k1 + k2 + k3 + k4)/6
-#solce diff eq by Leap Frog Method
+#solve diff eq by Leap Frog Method
 if diff_method == "LF": #doesnt work every time
-    i = 0
+    r_0[1] = r_0[1] + 0.5*h*vector(r_0,tdata[0])[1]
     for t in tdata:
-        rarray.append(r_0)
-        r_0[0] = r_0[0] + r_0[1]*t + 0.5*((numpy.pi/180)*(-9.81)/(length) * numpy.cos(r_0[0]*(numpy.pi / 180.)))*(t**2)
         thetadata.append(r_0[0])
-        r_0[1] = r_0[1] + 0.5*((numpy.pi/180)*(-9.81)/(length) * numpy.cos(rarray[i][0]*(numpy.pi / 180.))+(numpy.pi/180)*(-9.81)/(length) * numpy.cos(r_0[0]*(numpy.pi / 180.)))*t
         omegadata.append(r_0[1])
-        i += 1
+        r_0[0] += h*vector(r_0*(t+0.5*h),t+0.5*h)[0]
+        r_0[1] += h*vector(r_0*(t+h),t+h)[1]
+        
+        
 #Get x,y coords for bob, line, and time for gif
 j = 0 #indexing variable
 if gif_method == 1:    
@@ -88,6 +88,7 @@ if gif_method == 1:
 #p.plot(ltdata[50:],l_gifdata[10][50:])
 #plot  
 if gif_method==0:
+    print min(thetadata[500:600])
     p.title("Angle of NonLinear Pendulum Over Time")
     p.xlabel("Time (s)")
     p.ylabel("Angle of Pendulum Arm (degrees)")
